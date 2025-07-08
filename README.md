6420ee61179c020636554e3ccb0a5476da76732a
N <nsm4421@naver.com>
2025-07-08 23:09:28 +0900

    상품모델링 조회 화면 Mock Up UI

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ npm install slate slate-react
 
 npm install --save-dev @types/slate
 ```
+
+## 상품코드
+
+- 통합상품코드
+    - 대표상품코드
+        - 단위상품코드
+            - 단위상품이력코드
+
+- 알파1404
+    - 알파2501
+        - 알파2501
+        - 알파2504
+    - 엠바2501
+        - 알파2504
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { Move, MoveRight } from "lucide-react";
+import Link from "next/link";
+
+export default function HomePage() {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="flex justify-center h-full">
+      <div className="mx-auto my-auto">
+        <main>
+          <h1 className="mb-5 font-bold text-xl">SOL2.0 디지안 시안</h1>
+
+          <Link href={"/display-products"}>
+            <Button className="flex justify-between items-center p-2 cursor-pointer hover:text-sky-600">
+              <span>Display Products Page</span>
+              <MoveRight />
+            </Button>
+          </Link>
+        </main>
+      </div>
     </div>
   );
 }

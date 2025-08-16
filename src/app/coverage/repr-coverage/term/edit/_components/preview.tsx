40a7f94e1aade2af9872c7834f4f86c23d2b120d
N <nsm4421@naver.com>
2025-08-16 12:01:55 +0900

    필요없는 페이지 삭제
    지난 회의 때 대표담보 조회화면에서 slider로 pv, 약관 모델링하는 화면을 보여주는걸로 논의됨
    필요없는 페이지 삭제함

--- src/app/coverage/repr-coverage/term/edit/_components/preview.tsx
+++
@@ -1,5 +0,0 @@
-import { Card } from "@/components/ui/card";
-
-export default function Preview(){
-    return <Card>미리보기</Card>
-}
--- src/lib/term-editor/get-sibling-index.ts
+++
@@ -1,6 +0,0 @@
-import { Path } from "slate";
-
-/** 현재 li의 1-based 형제 인덱스 */
-export default function getSiblingIndex(path: Path) {
-  return (path[path.length - 1] as number) + 1;
-}
